c6ca01fe306daf4dd95fba3d608ec85a980f41ab
Константин Дьяченко <konard@me.com>
2019-11-01 23:45:33 +0300

    Initial code commit.

---
+++ ErrorListener.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+using Antlr4.Runtime;
+
+namespace AntlrExample
+{
+    internal class ErrorListener : IAntlrErrorListener<IToken>
+    {
+        public void SyntaxError(TextWriter output, IRecognizer recognizer, IToken offendingSymbol, int line, int charPositionInLine, string msg, RecognitionException e)
+        {
+            //throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using Antlr4.Runtime;
+using System.IO;
+
+namespace AntlrExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader fileStream = new StreamReader("Program.cs"))
+            {
+                AntlrInputStream inputStream = new AntlrInputStream(fileStream);
+
+                CSharpLexer lexer = new CSharpLexer(inputStream);
+                CommonTokenStream commonTokenStream = new CommonTokenStream(lexer);
+                CSharpParser parser = new CSharpParser(commonTokenStream);
+
+                parser.RemoveErrorListeners();
+                parser.AddErrorListener(new ErrorListener()); // add ours
+
+                var compilationUnit = parser.compilation_unit();
+
+            }
+        }
+    }
+}
